f04f20afac1bd9020190b0aa8d660f0e88e07fd5
swaraj <swarajdalmia.bitspilani@gmail.com>
2018-10-19 15:22:47 +0200

    Added the sliding window for the area and other processing steps

--- calc_slopes.py
+++ calc_slopes.py
@@ -7,10 +7,23 @@ from scipy import interpolate, stats
 import numpy.polynomial.polynomial as poly
 
 
+# to do : Remove the trials which have a really high std. Calculate the slopes for various window sizes
+# and plot the means and s.t.d. 
 
-def fix_blinks(y, trail_number, difficulty_cond):
+
+def pre_process(pupil_size, base_line):
+    # subtracts the average value of the baseline from all the entries of pupil_size
+    mean_base = np.mean(base_line)
+    pupil_size = [i - mean_base for i in pupil_size]
+    return pupil_size
+
+def fix_blinks(y, trail_number, difficulty_cond, task_type):
+    # interpolates the blinks
     x = range(len(y))
     x_non_zero = []
+
+    # interpolation doesn't take into account these points. 
+    # data points where is a jump in the pupil dilation are removed
     for i in range(1,len(y),1):
         if (y[i] == 0 or abs(y[i]-y[i-1]) > 0.005*y[i]):
             continue
@@ -18,64 +31,151 @@ def fix_blinks(y, trail_number, difficulty_cond):
 
     # x_non_zero = np.nonzero(y)[0]
 
+    # the y values for the x positions 
     y_non_zero = np.take(y,x_non_zero)
+
     x_new = np.arange(0,len(y),0.2)
 
-
+    # interpolation with a 3-d spline
     tck = interpolate.splrep(x_non_zero, y_non_zero, k = 3)  # a 3rd order spline
-    # y_new = interpolate.splev(x_new, tck, der=0)
-    # coefs = poly.polyfit(x_non_zero, y_non_zero, 3)
-    # y_new = poly.polyval(x_new, coefs)
+
+    # the new y values after interpolation 
     y_new = interpolate.splev(x, tck, der=0)
     y_new = [int(round(i)) for i in y_new]
-    return np.array(y_new)
-    # plt.plot(x, y, 'o', x_new, y_new)
-    # plt.title('Trial Number = ' + str(trail_number) + '  , Difficulty =' + str(difficulty_cond))
-    # plt.savefig('Plots/Daan/Trial_' + str(trail_number) + '_' + str(difficulty_cond)+ '_' + '.png')
+
+    # plt.plot(x, y, 'o', x, y_new)
+    # plt.title('Difficulty = ' + str(difficulty_cond) + ' , Task = ' + str(task_type))
+    # plt.savefig('Plots/Leon/Trial_' + str(trail_number) + '_' + str(difficulty_cond)+ '_' + '.png')
     # plt.clf()
+    return np.array(y_new)
 
-def main():
+def mean_slopes(trial_pupil_sizes, fixation_pupil_sizes, difficuly_order, task_type):
+    # calculates the mean, simple slopes and performs all the pre-processing steps
+    pupil_sizes = []
+    pupil_mean = [] 
+    slopes = []  
+    for i in range(len(trial_pupil_sizes)):
+        # Reads the pupil sizes from an array of strings
+        pupil_size = [int(float(x)) for x in trial_pupil_sizes[i].split(',')]
+        base_line = [int(float(x)) for x in fixation_pupil_sizes[i].split(',')]
 
-    data = pd.read_csv("/Users/vashisthdalmia/Documents/GitHub/usermodels/subject-swaraj.csv")
+        # Interpolates the blinks with a 3rd order spline - same as paper
+        pupil_size = fix_blinks(pupil_size, i, difficuly_order[i], task_type[i])
+        
+        base_line = fix_blinks(base_line, i, difficuly_order[i], task_type[i])
+
+        # the pre-processing step.
+        pupil_size = pre_process(pupil_size, base_line)
+
+        x = np.arange(0,len(pupil_size),1)
+        slope = stats.linregress(x, pupil_size)[0]
+
+        pupil_sizes.append(pupil_size)
+        pupil_mean.append(np.mean(pupil_size))
+        slopes.append(slope)
+    mean_slopes_data = []
+    mean_slopes_data.append(pupil_sizes)
+    mean_slopes_data.append(pupil_mean)
+    mean_slopes_data.append(slopes)
+
+    return mean_slopes_data
+
+def find_condition(difficulty_order, j, task_type, i):
+    # find the trials with difficulty = j and task type = i
+    find_pos = []
+    for x in range(len(difficulty_order)):
+        if (difficulty_order[x] == j and task_type[x] == i):
+            find_pos.append(x)
+    return find_pos
+
+def find_mean_areas(pupil_sizes, trials, window_size, window_slide):
+    # for a specific set of trails finds the mean areas with the sliding wondow 
+    mean_area = []
+    for j in range(0, len(pupil_sizes[trials[0]]) -  window_size, window_slide):
+        mean_trail = [] 
+        for i in trials :
+            ps = pupil_sizes[i]
+            mean_trail.append(find_window_area(ps, j, window_size))
+        mean_trial = remove_area_outliers(mean_trail)
+        mean_area.append(np.mean(mean_trail))
+    return mean_area
+    
+def find_window_area(pupil_size, start, window_size):
+    # finds the area under the window for a given array of pupil sizes !
+    sum = 0
+    for i in range(start, start+window_size ,1):
+        sum += pupil_size[i]
+    return sum 
+
+def remove_area_outliers(areas):
+    # removes the area outliers that are more than 2 std over the mean
+    new_areas = []
+    mean_area = np.mean(areas)
+    std_area = np.std(areas)
+    for i in areas:
+        if(abs(mean_area - i) < 2*std_area):
+            new_areas.append(i)
+    return new_areas
+
+def main(subject):
+    # the data you want to read
+    data = pd.read_csv("/Users/vashisthdalmia/Documents/GitHub/usermodels/subject-" + subject + ".csv")
     # data = pd.read_csv("/Users/vashisthdalmia/Documents/GitHub/usermodels/subject-daan.csv")
 
-    ps = data.str_pupilsizes
-    # print("Number of trials: ", len(ps_daan))
+    # stores the pupil sizes for all the trial periods
+    trial_pupil_sizes = data.str_pupilsizes
+    # stores the pupil sizes for all the fixation cross periods
+    fixation_pupil_sizes = data.str_baseline
+    # stores the difficulty order and the task_type
     difficuly_order = data.difficulty
-    # print(difficuly_order)
+    task_type = data.tasktype
 
-    pupil_sizes = []
-    slopes = []
-    intercept = []
-    for i in range(len(ps)):
-        print(i)
-        trial = [int(float(x)) for x in ps[i].split(',')]
-        fixed_pupil_data = fix_blinks(trial, i, difficuly_order[i])
-        pupil_sizes.append(fixed_pupil_data)
-        x = np.arange(0,len(fixed_pupil_data),1)
-        slope = stats.linregress(x, fixed_pupil_data)[0]
-        slopes.append(slope)
-
+    mean_slopes_data = mean_slopes(trial_pupil_sizes, fixation_pupil_sizes, difficuly_order, task_type)
     
+    pupil_sizes = np.array(mean_slopes_data[0])
+    pupil_mean= np.array(mean_slopes_data[1])
+    slopes = np.array(mean_slopes_data[2])
 
-    # pupil_sizes.append(fix_blinks(trial))
-
-
-    # ps_arithmetic_task = np.zeros((3,len(pupil_sizes[1])), dtype=float)
-    # ps_word_reversals = np.zeros((3,len(pupil_sizes[1])), dtype=float)
-
-
+    arr_task = ["arithmetic", "reversal"]
+    arr_condition = ["easy", "medium", "hard"]
+    struct_condition = np.zeros(shape=(2,3)).tolist()
+    area__mean_condition = np.zeros(shape=(2,3)).tolist()
+    window_size = 375 # 1000ms since pupil size is stored every 10ms
+    window_slide = 25 # 250ms
+    for i in range(len(arr_task)):
+        for j in range(len(arr_condition)):
+            struct_condition[i][j] = find_condition(difficuly_order, arr_condition[j], task_type, arr_task[i])
+            area__mean_condition[i][j] = find_mean_areas(pupil_sizes, struct_condition[i][j], window_size, window_slide)
     
-    for i in range(15):
-        if difficuly_order[i] == "easy":
-            print('reached easy')
-            plt.plot(0,slopes[i],'bo', label = 1)
-        elif difficuly_order[i] == "medium":
-            plt.plot(1,slopes[i],'go', label = 2)
-        else:
-            plt.plot(2,slopes[i],'ro', label = 3) 
-    plt.title("Swaraj : Slopes for Arithmetic tasks")
-    plt.savefig('Plots/Swaraj_Slopes_Arithmetic.png')
+    for i in range(len(arr_task)):
+        x = range(len(area__mean_condition[i][0]))
+        plt.plot(x,area__mean_condition[i][0],'bo', label = "easy")
+        plt.plot(x,area__mean_condition[i][1],'ro', label = "medium")
+        plt.plot(x,area__mean_condition[i][2],'go', label = "hard")
+        plt.legend(loc='upper left')
+        plt.title(subject + ", Task = " + str(arr_task[i]))
+        plt.savefig('Plots/'+ subject + '_Areas_' + str(arr_task[i]) + '.png')
+        plt.clf()
+
+
+    # for i in range(1,len(pupil_mean),1):
+    #     if(task_type[i] == "arithmetic"):
+    #         if difficuly_order[i] == "easy":
+    #             plt.plot(0,pupil_mean[i],'bo', label = 1)
+    #         elif difficuly_order[i] == "medium":
+    #             plt.plot(1,pupil_mean[i],'go', label = 2)
+    #         else:
+    #             plt.plot(2,pupil_mean[i],'ro', label = 3)
+    #     else:
+    #         if difficuly_order[i] == "easy":
+    #             print('reached easy')
+    #             plt.plot(3,pupil_mean[i],'bo', label = 4)
+    #         elif difficuly_order[i] == "medium":
+    #             plt.plot(4,pupil_mean[i],'go', label = 5)
+    #         else:
+    #             plt.plot(5,pupil_mean[i],'ro', label = 6)
+    # plt.title("Swaraj : Means. Arthmetic[0-2], Reversals[3-5]")
+    # plt.savefig('Plots/Swaraj_means.png')
     # plt.clf()
 
     # for i in range(15,30,1):
@@ -87,19 +187,17 @@ def main():
     #     else:
     #         ps_word_reversals[2,:] += pupil_sizes[i]
     
+    
+    # pos_to_del = []
+    # for i in range(0,len(pupil_mean),1):
+    #     if (abs(pupil_mean[i] - mean) > 2*std) :
+    #         pos_to_del.append(i)
 
-
-    # # pupilsizes_1 = 
-    # # for i in range()
-
-    # # print(len(pupil_swaraj[0]))
-    # plt.plot(ps_arithmetic_task[0], 'b-', label = "easy")
-    # plt.plot(ps_arithmetic_task[1], 'r-', label = "medium")
-    # plt.plot(ps_arithmetic_task[2], 'g-', label = "hard")
-
-    # plt.show()
-
+    # pupil_mean = np.delete(pupil_mean, (pos_to_del), axis=0)
+    # pupil_sizes = np.delete(pupil_sizes, (pos_to_del), axis=0)
+    # slopes = np.delete(slopes, (pos_to_del), axis=0)
     
 if __name__ == '__main__':
     # fix_blinks()
-    main()
+    main("swaraj")
+    main("leon")
